e74d72d2b87ac85cfe6018f57515765666e82925
Mohammad Algarni <m.t.algarni@gmail.com>
2025-11-24 20:49:36 +0300

    Updates main.py file.

--- src/main.py
+++ src/main.py
@@ -18,6 +18,14 @@ templates = Jinja2Templates(directory='src/templates')
 async def get_landing_page(request: Request):
     return templates.TemplateResponse("index_new.html", {"request": request, "STATIC_URL": STATIC_URL})
 
+@app.get("/terms")
+async def get_terms(request: Request):
+    return templates.TemplateResponse("terms.html", {"request": request, "STATIC_URL": STATIC_URL})
+
+@app.get("/privacy")
+async def get_privacy(request: Request):
+    return templates.TemplateResponse("privacy.html", {"request": request, "STATIC_URL": STATIC_URL})
+
 @app.get("/mode")
 def get_mode():
     # JSON-ready dict -> json.dumps({"Hello": "World"})
